7722251fdbf56dd0fb0b09239bd207a16af6041d
RaneemAlshahrani <s202277080@kfupm.edu.sa>
2026-03-30 12:10:22 +0300

    refactor: remove unnecessary variable declaration and enable CORS middleware

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -4,12 +4,11 @@ import express from "express";
 
 
 // TODO 2: Create the Express app and store it in a variable named app
-
+const app = express();
 
 
 // TODO 3: Allow React to access the 
-import cors from "cors";
-app.use(cors());
+
 
 
 
